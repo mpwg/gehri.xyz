f54771d28d373b73901b602f1e5c0cc0d4650639
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2026-03-09 09:16:22 +0100

    Make homepage fully CMS-driven

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Matthias Géhri';
+export const SITE_TITLE = 'Matthias Wallner-Géhri';
 export const SITE_DESCRIPTION =
-  'Persönliche Webseite von Matthias Géhri mit Kurzprofil, Kontakt und ausgewählten Projekten.';
+  'Persönliche Webseite von Matthias Wallner-Géhri mit Kurzprofil, Kontakt und ausgewählten Projekten.';
